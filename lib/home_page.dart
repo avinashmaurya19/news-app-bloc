3216cbb898324949fff1014c79a5782359f9891c
avinash <avinashmaurya539239@gmail.com>
2024-02-03 19:52:59 +0530

    up

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -139,15 +139,4 @@ class _HomePageState extends State<HomePage> {
         }));
   }
 
-  // void getNews() {
-  //   String baseApi =
-  //       'https://newsapi.org/v2/top-headlines?country=in&apiKey=58b98b48d2c74d9c94dd5dc296ccf7b6';
-  //   BlocProvider.of<NewsBloc>(context).add(GetNews(baseApi));
-  // }
-
-  // @override
-  // void initState() {
-  //   getNews();
-  //   super.initState();
-  // }
 }
